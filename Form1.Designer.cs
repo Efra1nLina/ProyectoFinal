226493b9d6939445464a2fa403dd9f8b6b25deb6
diego <diegolinares@umes.edu.gt>
2025-05-24 17:47:04 -0600

    combobox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            elegirZona = new ComboBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // elegirZona
+            // 
+            elegirZona.FormattingEnabled = true;
+            elegirZona.Items.AddRange(new object[] { "VIP", "Tribuna", "General", "Mesas Platino" });
+            elegirZona.Location = new Point(35, 73);
+            elegirZona.Name = "elegirZona";
+            elegirZona.Size = new Size(121, 23);
+            elegirZona.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Zona:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(elegirZona);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox elegirZona;
+        private Label label1;
     }
 }
